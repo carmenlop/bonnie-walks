111460af0a474c7f91832486b94b0d6f1a7b767e
carmenlop <cllopez92@gmail.com>
2025-01-07 10:07:14 -0800

    Basic Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen overflow-x-hidden overflow-y-scroll bg-red-500">
+      <div className="w-full flex flex-col gap-[12px] p-[16px]">
+        <div className="text-white text-3xl font-bold flex justify-center w-full py-[24px]">Bonnie's Walks</div>
+        <div className="flex flex-row justify-between items-center px-[16px]">
+          <Image src='/bonnie.png' width={150} height={150} alt="dog head" className="bg-red-600 rounded-full px-[30px] py-[15px]"/>
+          <div className="flex flex-col justify-center items-center bg-red-600 text-white rounded-full w-[150px] h-[150px]">
+            <div className="text-l">Total Time</div>
+            <div className="text-2xl font-bold">300h 39m</div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="self-center flex flex-col justify-center items-center bg-red-600 text-white rounded-full w-[150px] h-[150px]">
+            <div className="text-4xl font-bold">Start</div>
+        </div>
+        <table className="table-auto border-collapse border border-slate-400 mt-[50px]">
+          <thead className="bg-red-600">
+            <tr>
+              <th className="border border-red-700 text-white py-[6px]">Date</th>
+              <th className="border border-red-700 text-white py-[6px]">Start</th>
+              <th className="border border-red-700 text-white py-[6px]">End</th>
+              <th className="border border-red-700 text-white py-[6px]">Total</th>
+            </tr>
+          </thead>
+          <tbody className="bg-red-400">
+            <tr>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">1/6/25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">8:25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:00</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">35m</td>
+            </tr>
+            <tr>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">1/5/25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">8:40</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:15</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">35m</td>
+            </tr>
+            <tr>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">1/4/25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:00</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:15</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">15m</td>
+            </tr>
+            <tr>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">1/3/25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:00</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:15</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">15m</td>
+            </tr>
+            <tr>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">1/2/25</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:00</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">9:15</td>
+              <td className="border border-red-700 text-center text-md text-white font-bold py-[6px]">15m</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
